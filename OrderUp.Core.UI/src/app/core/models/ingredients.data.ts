7612a90d3ca3ecb60984826ed9fe22c4f87fc89c
soflyj <soflyj@gmail.com>
2025-06-14 11:34:13 +0200

    test

---
+++ OrderUp.Core.UI/src/app/core/models/ingredients.data.ts
@@ -0,0 +1,16 @@
+import { Ingredients } from './ingredients.interface';
+
+export const INGREDIENTS: Ingredients[] = [
+  {
+    id: 1,
+    name: 'Milk'
+  },
+  {
+    id: 2,
+    name: 'Flour'
+  },
+  {
+    id: 3,
+    name: 'Icing'
+  },
+];
